63875c1f908284204f6f93a69ca73c40af7ff092
tfuquirk <timothyfquirk@gmail.com>
2018-12-11 15:48:06 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+# create a Dog class with initialized variables of name and breed(with a defalult of "mutt"
+
+class Dog
+
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed 
+  end
+
+
+end
