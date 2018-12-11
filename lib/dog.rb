7fc9ee5d57117359ade9dc01f729fd75d9f7d3c4
diaaanek <dkorongy@gmail.com>
2018-12-11 16:12:39 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+def initialize(name,"Mutt")
+  @name=name
+  @breed="Mutt"
+end
+
+def name=(name)
+  @name=name
+end
+
+def breed=(breed)
+  @breed=breed
+end
+
+  def name
+    @name
+  end
+def breed
+  @breed
+end
+end
